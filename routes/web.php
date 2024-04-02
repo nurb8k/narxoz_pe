0c475c44a50960c2ecde4d2d99ada2626b995070
emdommed <nurb8k@gmail.com>
2024-04-02 21:54:33 +0500

    test222h

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd('Hello hjfgjhfgjfWorld');
+    dd('Hellofghjkl; hjfgjhfgjfWorld');
     return view('welcome');
 });
